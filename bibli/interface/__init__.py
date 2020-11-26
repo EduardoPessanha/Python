b092a2aa7ba7a9a4db5456ce981fb0cd16b44c18
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-26 08:19:01 -0300

    Criado o arquivo cadcli.py e novas funções no módulo interface

--- bibli/interface/__init__.py
+++ bibli/interface/__init__.py
@@ -1,11 +1,32 @@
 def linha(simb='-', tam=40):
-    return simb * tam
+	return simb * tam
 
 
 def texto(msg='', simb='-'):
-    tam = len(msg)+8
-    print(linha(simb, tam))
-    # print(f'{simb}{msg.upper().center(tam-2)}{simb}')
-    print(msg.upper().center(tam))
-    print(linha(simb, tam))
-    return
+	tam = len(msg) + 8
+	print(linha(simb, tam))
+	# print(f'{simb}{msg.upper().center(tam-2)}{simb}')
+	print(msg.upper().center(tam))
+	print(linha(simb, tam))
+	return
+
+
+def menu(lista=''):
+	""" Recebe os itens do menu
+	:param lista: lista contendo os itens do menu
+	:return: retorna a opção escolhida no menu
+	"""
+	cab('menu principal', '*')
+	print(linha('*'))
+	for item in lista:
+		print(f'{"*":<3}{item}{"*":>{37 - len(item)}}')
+	print(linha('*'))
+	opcao = str(input('Sua opção: '))
+	return opcao
+
+
+def cab(txt='', simb='=', tam=40):
+	print(linha(simb))
+	print(txt.upper().center(tam))
+	print(linha(simb))
+	return
---
+++ cadcli.py
@@ -0,0 +1,8 @@
+from bibli.interface import menu
+# import bibli.interface as bib
+
+opc = ['1- Ver clientes cadastrados',
+	   '2- Cadastrar novo cliente',
+	   '3- Sair do sistema'
+	   ]
+menu(opc)
