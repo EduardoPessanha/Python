6f78912c58a2ec945bc775dbf42bb73b74843a5e
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-27 09:26:09 -0300

    Atualização de  arquivo

--- bibli/interface/__init__.py
+++ bibli/interface/__init__.py
@@ -1,32 +1,66 @@
+from bibli.numero import leiafloat
+
+
 def linha(simb='-', tam=40):
-	return simb * tam
+    return simb * tam
 
 
 def texto(msg='', simb='-'):
-	tam = len(msg) + 8
-	print(linha(simb, tam))
-	# print(f'{simb}{msg.upper().center(tam-2)}{simb}')
-	print(msg.upper().center(tam))
-	print(linha(simb, tam))
-	return
+    tam = len(msg) + 8
+    print(linha(simb, tam))
+    # print(f'{simb}{msg.upper().center(tam-2)}{simb}')
+    print(msg.upper().center(tam))
+    print(linha(simb, tam))
+    return
 
 
-def menu(lista=''):
-	""" Recebe os itens do menu
-	:param lista: lista contendo os itens do menu
-	:return: retorna a opção escolhida no menu
-	"""
-	cab('menu principal', '*')
-	print(linha('*'))
-	for item in lista:
-		print(f'{"*":<3}{item}{"*":>{37 - len(item)}}')
-	print(linha('*'))
-	opcao = str(input('Sua opção: '))
-	return opcao
+def menu(lista='', titulo='menu principal', op=' '):
+    """ Recebe os itens do menu
+    :param lista: lista contendo os itens do menu
+    :param titulo: Título do menu
+    :param op: opções disponíveis
+    :return: retorna a opção escolhida no menu
+    """
+    cab(titulo, '*')
+    print(linha('*'))
+    for item in lista:
+        print(f'{"*":<3}{item}{"*":>{37 - len(item)}}')
+    print(linha('*'))
+    if op != ' ':
+        while True:
+            opcao = str(input(f'Escolha a Sua opção ({op}): ')).upper()
+            if opcao not in op:
+                print(f'\033[1;31mERRO: Opção \"INVÁLIDA\". Tente outra vez.\033[m')
+            else:
+                break
+    else:
+        opcao = str(input(f'Escolha a Sua opção: '))
+    return opcao
 
 
 def cab(txt='', simb='=', tam=40):
-	print(linha(simb))
-	print(txt.upper().center(tam))
-	print(linha(simb))
-	return
+    print(linha(simb))
+    print(txt.upper().center(tam))
+    print(linha(simb))
+    return
+
+
+def calcohm(op=''):
+    res = 0
+    if op == 'V':
+        i = leiafloat('Valor da corrente em Ampere: ')
+        r = leiafloat('Valor da resistência em Ohm:')
+        v = i * r
+        res = f'O valor da tensão é {v:.2f} Volts'
+    elif op == 'I':
+        v = leiafloat('Valor da tensão em Volts: ')
+        r = leiafloat('Valor da resistência em Ohm:')
+        i = v / r
+        res = f'O valor da corrente é {i:.2f} Amperes'
+    elif op == 'R':
+        i = leiafloat('Valor da corrente em Ampere: ')
+        v = leiafloat('Valor da tensão em Volts:')
+        r = v / i
+        res = f'O valor da resistência é {v:.2f} Ohms'
+    print('Erro desconhecido')
+    return res
--- leideohm.py
+++ leideohm.py
@@ -1,55 +1,18 @@
-# Desafio: reescrever o código, usando a tecnica de modulos!!!!
-azul = '\033[1;7;34m'
-neutro = '\033[m'
-titulo = " Lei de Ohm em Corrente Contínua "
-print(f"\n{azul}{titulo:^42}{neutro}\n")
-fim = ' '
-linha = '-=' * 21
-while fim not in 'N':
-    print('''******************************************
-*   Qual a unidade você quer calcular:   *
-*                                        *
-*            V = Tensão                  *
-*            I = Corrente                *
-*            R = Resistência             *
-*                                        *
-******************************************''')
-    while True:
-        opcao = str(input('Escolha a sua opção (V/I/R): ')).upper().strip()[0]
-        if opcao not in 'VIR':
-            print('Entrada INVÁLIDA.', end=' ')
-        else:
-            break
-    if opcao == 'V':
-        print(f'\n{linha}\n')
-        I = float(input('Informe o valor da corrente em Aperes: '))
-        R = float(input('Informe o valor da resistência em Ohms: '))
-        V = I * R
-        print(f'O valor da tensão é {V:.2f} Volts\n')
-        print(f'{linha}\n')
-    elif opcao == 'I':
-        print(f'\n{linha}\n')
-        V = float(input('Informe o valor da tensão em Volts: '))
-        R = float(input('Informe o valor da resistência em Ohms: '))
-        I = V / R
-        print(f'O valor da corrente é {I:.2f} Aperes\n')
-        print(f'{linha}\n')
-    elif opcao == 'R':
-        print(f'\n{linha}\n')
-        V = float(input('Informe o valor da tensão em Volts: '))
-        I = float(input('Informe o valor da corrente em Aperes: '))
-        R = V / I
-        print(f'O valor da resistência é {R:.2f} Ohms\n')
-        print(f'{linha}\n')
-    while True:
-        fim = str(input('Deseja continuar (S/N): ')).upper().strip()[0]
-        if fim not in 'SN':
-            print('Opção INVÁLIDA.', end=' ')
-        else:
-            break
-    if fim == 'N':
-        print()
-        break
-    else:
-        print(f'{linha}\n')
+from bibli.interface import menu, linha, calcohm
+from time import sleep
 
+item = [f"{'Ecolha a unidade para calcular':>32}".upper(),
+        ' ', f"{'V = Tensão':>18}",
+        f"{'I = Corrente':>20}",
+        f"{'R = Resistência':>23}",
+        f"{'S = Sair do sistema':>27}", ' ']
+titulo = " Lei de Ohm em Corrente Contínua "
+
+while True:
+    opcao = menu(item, titulo, op='V/I/R/S')
+    if opcao == 'S':
+        print(linha('*'))
+        print('Saindo do sistema... Até logo!')
+        break
+    print(calcohm(opcao))
+    sleep(1)
