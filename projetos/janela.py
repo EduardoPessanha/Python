eee5e19cf375043eccab6e126c700e8b0115a333
Eduardo Antonio Moreira Pessanha <68357896+EduardoPessanha@users.noreply.github.com>
2021-10-11 23:33:49 -0300

    Update janela.py
    Alteração e remanejamentos feito no desing da Tela!

--- projetos/janela.py
+++ projetos/janela.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-# -*-coding: UTF-8-*-
+# -*-coding:UTF-8-*-
 """
 
 Protótipo de layout de interface gráfica para cadastro de contatos
 
 """
-# import tkinter as tk
 from tkinter import Button, Entry, Text, Frame, LabelFrame
 from tkinter import PanedWindow, Label, mainloop, Tk, ttk
 
@@ -15,7 +14,6 @@ from tkinter import font, Tk
 root = Tk()
 print(font.families())
 """
-
 
 app = Tk()
 app.title("Protótipo de Interface Gráfica - Cadastro")
@@ -32,39 +30,29 @@ style.theme_use('classic')
 # -Criando uma janela panorâmica:
 # , background="#0000ee", bg='blue'
 pw = PanedWindow(app, orient='vertical', bg="grey")
-pw.pack(fill='both', expand=True, side='bottom', padx=(2.5, 0))
-# pw = ""
+pw.pack(fill='y', expand=0, side='top', padx=(2.5, 0))
 
 # *Criando os Widgets da janela:
 
 # -Widget quadros (Frame/LabelFrame):
 # relief => flat(padrão), groove, raised, ridge, solid, ou sunken
-# fr_sup = Frame(pw, borderwidth=2, relief='raised', border=1)
-
-# lf_inf = LabelFrame(pw, text='Inferior', relief='sunken',
-#                     border=1, borderwidth=1)
-
-lfr_contato = LabelFrame(pw, text='CONTATOS',
+lfr_contato = LabelFrame(app, text='CONTATOS',
                          relief='ridge',
                          borderwidth=1,
                          border=1,
                          font=('Ebrima', 10, 'bold'))
-
-# fr_btn_sup = Frame(fr_sup, border=2, borderwidth=2, relief='sunken')
-
-fr_contato = Frame(lfr_contato, borderwidth=1, relief='sunken')
-
-lfr_pesquisar = LabelFrame(fr_contato, text='Pesquisar',
+lfr_formulario = LabelFrame(app, text='EDITAR CONTATOS', borderwidth=1,
+                            relief='ridge', padx=10, pady=(0), font=('Ebrima', 10, 'bold'))
+lfr_pesquisar = LabelFrame(lfr_formulario, text='Pesquisar',
                            font=('Ebrima', 10, 'bold'),
                            relief='sunken', border=1,
-                           borderwidth=1, labelanchor='n'
-                           )
-
-lfr_editar = LabelFrame(fr_contato, text='Editar',
+                           borderwidth=1, labelanchor='n')
+lfr_editar = LabelFrame(lfr_formulario, text='Editar',
                         font=('Ebrima', 10, 'bold'),
                         relief='sunken', border=1,
-                        borderwidth=2, labelanchor='n')
+                        borderwidth=1, labelanchor='n')
 
+fr_contato = Frame(lfr_contato, borderwidth=1, relief='sunken')
 fr_btn_pesquisar = Frame(lfr_pesquisar, border=2,
                          borderwidth=2, relief='sunken')
 fr_btn_inserir = Frame(lfr_editar, border=2, borderwidth=2, relief='sunken')
@@ -74,56 +62,49 @@ fr_btn_reset = Frame(lfr_editar, border=2, borderwidth=2, relief='sunken')
 fr_btn_sair = Frame(lfr_editar, border=2, borderwidth=2, relief='sunken')
 
 # -Widget rótulo (Label):
-lbl_nome = Label(fr_contato, text='Nome:', width=8, anchor='w')
-lbl_fone = Label(fr_contato, text='Telefone:', width=8, anchor='w')
-lbl_mail = Label(fr_contato, text='E-mail:', width=8, anchor='w')
-lbl_end = Label(fr_contato, text='Endereço:', width=8, anchor='w')
-lbl_cid = Label(fr_contato, text='Cidade:', width=8, anchor='w')
-lbl_uf = Label(fr_contato, text='Estado:', width=8, anchor='w')
-lbl_obs = Label(fr_contato, text='Nota:', width=4, anchor='w')
+lbl_nome = Label(lfr_formulario, text='Nome:', width=8, anchor='w')
+lbl_fone = Label(lfr_formulario, text='Telefone:', width=8, anchor='w')
+lbl_mail = Label(lfr_formulario, text='E-mail:', width=8, anchor='w')
+lbl_end = Label(lfr_formulario, text='Endereço:', width=8, anchor='w')
+lbl_cid = Label(lfr_formulario, text='Cidade:', width=8, anchor='w')
+lbl_uf = Label(lfr_formulario, text='Estado:', width=8, anchor='w')
+lbl_obs = Label(lfr_formulario, text='Nota:', width=4, anchor='w')
 lbl_nome1 = Label(lfr_pesquisar, text='Nome:', width=5, anchor='w')
 
 # -Widget texto (Entry):
-txt_nome = Entry(fr_contato, width=25,
+txt_nome = Entry(lfr_formulario, width=25,
                  font=('Ebrima', 12), bd=1, justify='left', )
-txt_fone = Entry(fr_contato, width=15,
+txt_fone = Entry(lfr_formulario, width=15,
                  font=('Ebrima', 12), bd=1, justify='left')
-txt_mail = Entry(fr_contato, width=25,
+txt_mail = Entry(lfr_formulario, width=25,
                  font=('Ebrima', 12), bd=1, justify='left')
-txt_end = Entry(fr_contato, width=30,
+txt_end = Entry(lfr_formulario, width=30,
                 font=('Ebrima', 12), bd=1, justify='left')
-txt_cid = Entry(fr_contato, width=25,
+txt_cid = Entry(lfr_formulario, width=25,
                 font=('Ebrima', 12), bd=1, justify='left')
-txt_uf = Entry(fr_contato, width=2,
+txt_uf = Entry(lfr_formulario, width=2,
                font=('Ebrima', 12), justify='left')
-txt_obs = Text(fr_contato, width=30, height=4,
+txt_obs = Text(lfr_formulario, width=30, height=4,
                font=('Ebrima', 12))
-txt_nome1 = Entry(lfr_pesquisar, width=20,
-                  font=('Ebrima, 12'), justify='left')
+txt_nome1 = Entry(lfr_pesquisar, width=19,
+                  font=('Ebrima, 12'), justify='left')  # ***
 
 # -Widget botões (Button):
-# btn = Button(fr_btn_sup, text='TESTE', relief='raised', border=2)
-
-btn_pesquisar = Button(fr_btn_pesquisar, text='Pesquisar', width=10, bg='#b8b1e0',
+btn_pesquisar = Button(fr_btn_pesquisar, text='Pesquisar', width=10, bg='#b8b1e0', height=1,
                        font=('', '10', 'bold'), cursor='hand1', border=3, relief='raised',
-                       command='lambda: pesquisa(treev, txt_nome1)')
-
+                       command='lambda: pesquisa(treev, txt_nome1)', pady=1)
 btn_inserir = Button(fr_btn_inserir, text='Inserir', width=10, bg='#b8b1e0',
                      font=('', '10', 'bold'), cursor='hand1', border=3,
                      relief='raised', command='lambda: inserir(reglist)')
-
 btn_editar = Button(fr_btn_editar, text='Alterar', width=10, bg='#b8b1e0',
                     font=('', '10', 'bold'), cursor='hand1', border=3,
                     relief='raised', command="lambda: dml('editar'), state='disabled'")
-
 btn_excluir = Button(fr_btn_excluir, text='Excluir', width=10, bg='#b8b1e0',
                      font=('', '10', 'bold'), cursor='hand1', border=3, relief='raised',
                      command="lambda: dml('excluir'), state='disabled'")
-
 btn_reset = Button(fr_btn_reset, text='Atualizar', width=10, bg='#b8b1e0',
                    font=('', '10', 'bold'), cursor='hand1', border=3, relief='raised',
                    command='atualiza')
-
 btn_sair = Button(fr_btn_sair, text='Sair', width=10, bg='#b8b1e0',
                   font=('', '10', 'bold'), cursor='hand1', border=3,
                   relief='raised', command=exit)
@@ -136,7 +117,7 @@ colunas = ('id', 'nome', 'telefone', 'e-mail',
 
 # -Widget Treeview:
 treev = ttk.Treeview(fr_contato, columns=colunas,
-                     show='headings', height=14, padding=(1, 1))
+                     show='headings', height=14)
 
 # *- ****** Definindo os cabeçalhos das colunas ******
 treev.heading('id', text='ID')
@@ -165,22 +146,22 @@ treev.column('cidade', width=100, minwidth=0, stretch=True)
 treev.column('estado', width=35, minwidth=0, stretch=True, anchor='center')
 treev.column('observacao', width=230, minwidth=0, stretch=True)
 
-# -Posicionando os Widgets quadros, rótulos, entrada de textos e botões:
+# _Posicionando os Widgets quadros, rótulos, entrada de textos, botões e Treeview:
 
 # *Quadros(Frame/LabelFrame):
-# fr_sup.pack(fill='x', expand=True, anchor='n', side='top')
 lfr_contato.pack(fill='both', expand=True, anchor='s', side='top',
-                 ipady=125, pady=5)  # side => top, bottom, left, or right
-# lf_inf.pack(fill='both', expand=True, anchor='s', ipady=125)
-# fr_btn_sup.pack(anchor='w')
+                 ipady=5, pady=5)  # side => top, bottom, left, or right
+lfr_formulario.pack(fill='both', expand=True, side='top',
+                    padx=(5, 0), pady=5, ipady=0,)
+lfr_pesquisar.grid(column=4, row=5, columnspan=2, padx=(3, 0),
+                   pady=(10, 10), sticky='w')
+lfr_editar.grid(column=0, row=5, columnspan=4, padx=(6, 3),
+                pady=(10, 10), ipady=5, sticky='w', ipadx=0)
+
 fr_contato.pack(fill='both', expand=True, side='top',
-                padx=(2, 0), pady=11, ipady=5)
-lfr_pesquisar.grid(column=0, row=5, columnspan=3, padx=(1, 0),
-                   pady=(20, 2), sticky='w')
-lfr_editar.grid(column=2, row=5, columnspan=5, padx=(5, 0),
-                pady=(20, 2), ipady=5, sticky='n', ipadx=0)
-fr_btn_pesquisar.grid(column=3, row=0, rowspan=2, padx=10,
-                      pady=(0, 5), ipady=5, sticky='nsew')  # **
+                padx=(2, 0), pady=3, ipady=5,)
+fr_btn_pesquisar.grid(column=3, row=0, rowspan=2, padx=5,
+                      pady=(0, 5), sticky='nsew')
 fr_btn_inserir.grid(column=0, row=0, padx=10, pady=(5, 0), sticky='nsew')
 fr_btn_editar.grid(column=1, row=0, padx=5, pady=(5, 0), sticky='nsew')
 fr_btn_excluir.grid(column=2, row=0, padx=10, pady=(5, 0), sticky='nsew')
@@ -188,40 +169,44 @@ fr_btn_reset.grid(column=3, row=0, padx=5, pady=(5, 0), sticky='nsew')
 fr_btn_sair.grid(column=4, row=0, padx=10, pady=(5, 0), sticky='nsew')
 
 # *Rótulos(Label):
-lbl_nome.grid(column=0, row=2, pady=(20, 2))
+lbl_nome.grid(column=0, row=2, pady=(10, 2))
 lbl_fone.grid(column=0, row=3)
 lbl_mail.grid(column=0, row=4)
-lbl_end.grid(column=2, row=2, pady=(20, 2))
+lbl_end.grid(column=2, row=2, pady=(10, 2))
 lbl_cid.grid(column=2, row=3)
 lbl_uf.grid(column=2, row=4)
-lbl_obs.grid(column=4, row=2, pady=(20, 2))
+lbl_obs.grid(column=4, row=2, pady=(10, 2))
 lbl_nome1.grid(column=0, row=0, padx=(6, 0), sticky='w')
 
 # *Entrada de textos:
-txt_nome.grid(column=1, row=2, padx=5, pady=(20, 2), sticky='w')
+txt_nome.grid(column=1, row=2, padx=5, pady=(10, 2), sticky='w')
 txt_fone.grid(column=1, row=3, padx=5, pady=2, sticky='w')
 txt_mail.grid(column=1, row=4, padx=5, pady=2, sticky='w')
-txt_end.grid(column=3, row=2, padx=5, pady=(20, 2), sticky='w')
+txt_end.grid(column=3, row=2, padx=5, pady=(10, 2), sticky='w')
 txt_cid.grid(column=3, row=3, padx=5, pady=2, sticky='w')
 txt_uf.grid(column=3, row=4, padx=5, pady=2, sticky='w')
 txt_obs.grid(column=5, row=2, padx=(5, 0),
-             pady=(20, 2), sticky='wn', rowspan=3)
+             pady=(10, 2), sticky='wn', rowspan=3)
 txt_nome1.grid(column=0, row=1, padx=2, pady=5, sticky='nsew')
 
 # *Botões:
-# btn.grid(column=0, row=0, ipadx=30, padx=2, pady=2, sticky='n')
-btn_pesquisar.pack(anchor="center",
-                   fill='x', side='left')    # grid(column=3, row=0, padx=10, pady=2, sticky='n') # **
-btn_inserir.pack()    # grid(column=0, row=0, padx=10, pady=2, sticky='nsew')
-btn_editar.pack()     # grid(column=1, row=0, padx=5, pady=2, sticky='nsew')
-btn_excluir.pack()    # grid(column=2, row=0, padx=10, pady=2, sticky='nsew')
-btn_reset.pack()      # grid(column=3, row=0, padx=5, pady=2, sticky='nsew')
-btn_sair.pack()       # grid(column=4, row=0, padx=10, pady=2, sticky='nsew')
+btn_pesquisar.pack(anchor="center", ipady=1,
+                   fill='x', side='left')
+btn_inserir.pack()
+btn_editar.pack()
+btn_excluir.pack()
+btn_reset.pack()
+btn_sair.pack()
 
 # *Treeview:
-treev.grid(column=0, row=0, pady=(1, 5), columnspan=6, sticky='ew')
-scrollbarv.grid(row=0, column=6, sticky='n')
+treev.grid(column=0, row=0, padx=(10, 10),
+           pady=(3, 3), columnspan=6, sticky='nsew')
+scrollbarv.grid(row=0, column=6, sticky='ns')
 scrollbarh.grid(row=1, columnspan=6, sticky='ew')
+
+# *PanedWindow:
+pw.add(lfr_contato)
+pw.add(lfr_formulario)
 
 # -Incializando a janela:
 mainloop()
