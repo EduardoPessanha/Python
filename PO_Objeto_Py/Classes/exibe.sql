1e0af2f67177b40462d1bd47ae0f4623475d958f
Eduardo Antonio Moreira Pessanha <68357896+EduardoPessanha@users.noreply.github.com>
2021-11-28 08:51:45 -0300

    Commit Inicial

---
+++ PO_Objeto_Py/Classes/exibe.sql
@@ -0,0 +1,1 @@
+SELECT * FROM nome_tabela ORDER BY Data;
---
+++ PO_Objeto_Py/Classes/insere.sql
@@ -0,0 +1,4 @@
+INSERT INTO nome_tabela
+            (Data, Tipo, Descricao, Valor, Saldo, Obs)
+            VALUES(?, ?, ?, ?, ?, ?)
+            
